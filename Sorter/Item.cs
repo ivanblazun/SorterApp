80c6af2a924ab1f1696431a4f90d1a515b3b76bc
Ivan Blazun <87031660+ivanblazun@users.noreply.github.com>
2023-04-15 11:25:39 +0200

    List commit

---
+++ Sorter/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorter
+{
+    public class Item
+    {
+        public Item(int id, string name,int value)
+        {
+            Id= id;
+            Name= name; 
+            Value= value;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+    }
+}
--- Sorter/Program.cs
+++ Sorter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sorter
 {
@@ -6,7 +7,56 @@ namespace Sorter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Item> FullList = new List<Item>();
+          
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Item item = new Item(i, "item" + i, 1);
+                    FullList.Add(item);
+                }
+                else 
+                {
+                    Item item = new Item(i, "item" + (i-1), 2);
+                    FullList.Add(item);
+                }
+                 
+            }
+            List<Item> FullListSorted = new List<Item>();
+
+
+
+            for (int i = 0; i < FullList.Count-1; i++)
+            {
+                if (FullList[i].Value < FullList[i + 1].Value)
+                {
+                    FullListSorted.Add(FullList[i]);
+                }
+         
+            }
+            for (int x = 0; x < FullList.Count - 1; x++)
+            {
+                if (FullList[x].Value == 2)
+                {
+                    FullListSorted.Add(FullList[x]);
+                }
+            }
+
+            foreach (Item item in FullList) 
+            {
+                Console.WriteLine("Index: "+item.Id.ToString() + "  Item pair: " + item.Name.ToString() + "  Item type " + item.Value.ToString());
+            }
+
+            Console.WriteLine();
+
+            foreach (Item item in FullListSorted)
+            {
+                Console.WriteLine("Index: " + item.Id.ToString() + "  Item pair: " + item.Name.ToString() + "  Item type " + item.Value.ToString());
+            }
+
+            Console.ReadLine();
         }
     }
 }
